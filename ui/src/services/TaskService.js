6580e047325a4e26e1ec248a36a65473bf860a9a
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-11-15 15:07:34 -0300

    feat: adiciona baseUrl para dar o fetch

--- ui/src/services/TaskService.js
+++ ui/src/services/TaskService.js
@@ -1,34 +1,36 @@
-export async function getAllTasks() {
+const API_URL = process.env.REACT_APP_API_URL
 
-    const response = await fetch('/api/tasks');
-    return await response.json();
+export async function getAllTasks() {
+  const response = await fetch(`${API_URL}/api/tasks`)
+  return await response.json()
 }
 
 export async function createTask(data) {
-    const response = await fetch(`/api/task`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({task: data})
-    })
-    return await response.json();
+  const response = await fetch(`${API_URL}/api/task`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ task: data }),
+  })
+  return await response.json()
 }
 
 export async function deleteTask(taskId) {
-    const response = await fetch(`/api/task/${taskId}`, {method: 'DELETE'})
-    return await response.json();
+  const response = await fetch(`${API_URL}/api/task/${taskId}`, {
+    method: 'DELETE',
+  })
+  return await response.json()
 }
 
 export async function editTask(data) {
-    const response = await fetch(`/api/task`, {
-        method: 'PUT',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({task: data})
-    })
-    return await response.json();
+  const response = await fetch(`${API_URL}/api/task`, {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ task: data }),
+  })
+  return await response.json()
 }
 
 export async function fetchSettings() {
-
-    const response = await fetch('/api/settings');
-    return await response.json();
+  const response = await fetch(`${API_URL}/api/settings`)
+  return await response.json()
 }
